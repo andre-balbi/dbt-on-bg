177d4046fb55aafc7feefde3a4cd0b1cc859188a
drebalbi <nenhum@nenhum.com>
2025-08-14 12:22:04 -0300

    Added github workflows

--- models/analytics_obt/obt_product_inventory.sql
+++ models/analytics_obt/obt_product_inventory.sql
@@ -1,43 +1,43 @@
 WITH source AS (
     SELECT
         c.customer_id,
-        c.company         AS customer_company,
-        c.last_name       AS customer_last_name,
-        c.first_name      AS customer_first_name,
-        c.email_address   AS customer_email_address,
-        c.job_title       AS customer_job_title,
-        c.business_phone  AS customer_business_phone,
-        c.home_phone      AS customer_home_phone,
-        c.mobile_phone    AS customer_mobile_phone,
-        c.fax_number      AS customer_fax_number,
-        c.address         AS customer_address,
-        c.city            AS customer_city,
-        c.state_province  AS customer_state_province,
+        c.company AS customer_company,
+        c.last_name AS customer_last_name,
+        c.first_name AS customer_first_name,
+        c.email_address AS customer_email_address,
+        c.job_title AS customer_job_title,
+        c.business_phone AS customer_business_phone,
+        c.home_phone AS customer_home_phone,
+        c.mobile_phone AS customer_mobile_phone,
+        c.fax_number AS customer_fax_number,
+        c.address AS customer_address,
+        c.city AS customer_city,
+        c.state_province AS customer_state_province,
         c.zip_postal_code AS customer_zip_postal_code,
-        c.country_region  AS customer_country_region,
-        c.web_page        AS customer_web_page,
-        c.notes           AS customer_notes,
-        c.attachments     AS customer_attachments,
+        c.country_region AS customer_country_region,
+        c.web_page AS customer_web_page,
+        c.notes AS customer_notes,
+        c.attachments AS customer_attachments,
 
         e.employee_id,
-        e.last_name       AS employee_unique_employee_id,
-        e.company         AS employee_company,
-        e.last_name       AS employee_last_name,
-        e.first_name      AS employee_first_name,
-        e.email_address   AS employee_email_address,
-        e.job_title       AS employee_job_title,
-        e.business_phone  AS employee_business_phone,
-        e.home_phone      AS employee_home_phone,
-        e.mobile_phone    AS employee_mobile_phone,
-        e.fax_number      AS employee_fax_number,
-        e.address         AS employee_address,
-        e.city            AS employee_city,
-        e.state_province  AS employee_state_province,
+        e.last_name AS employee_unique_employee_id,
+        e.company AS employee_company,
+        e.last_name AS employee_last_name,
+        e.first_name AS employee_first_name,
+        e.email_address AS employee_email_address,
+        e.job_title AS employee_job_title,
+        e.business_phone AS employee_business_phone,
+        e.home_phone AS employee_home_phone,
+        e.mobile_phone AS employee_mobile_phone,
+        e.fax_number AS employee_fax_number,
+        e.address AS employee_address,
+        e.city AS employee_city,
+        e.state_province AS employee_state_province,
         e.zip_postal_code AS employee_zip_postal_code,
-        e.country_region  AS employee_country_region,
-        e.web_page        AS employee_web_page,
-        e.notes           AS employee_notes,
-        e.attachments     AS employee_attachments,
+        e.country_region AS employee_country_region,
+        e.web_page AS employee_web_page,
+        e.notes AS employee_notes,
+        e.attachments AS employee_attachments,
 
         p.product_id,
         p.product_code,
@@ -77,11 +77,12 @@ WITH source AS (
         CURRENT_TIMESTAMP() AS insertion_timestamp
     FROM {{ ref('fact_purchase') }} AS po
     LEFT JOIN {{ ref('dim_customer') }} AS c
-      ON c.customer_id = po.customer_id
+        ON po.customer_id = c.customer_id
     LEFT JOIN {{ ref('dim_employee') }} AS e
-      ON e.employee_id = po.employee_id
+        ON po.employee_id = e.employee_id
     LEFT JOIN {{ ref('dim_product') }} AS p
-      ON p.product_id = po.product_id
+        ON po.product_id = p.product_id
 )
+
 SELECT *
 FROM source
